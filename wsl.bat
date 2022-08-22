de6f009ab3c2fe2ba9988923838c551073a5ecaa
Jiang Jinming <360104322@qq.com>
2022-08-22 15:13:09 +0800

    feat(wsl):update wsl bat script

--- wsl.bat
+++ wsl.bat
@@ -1,31 +1,41 @@
+::with out console
+if "%1"=="hide" goto CmdBegin
+  start mshta vbscript:createobject("wscript.shell").run("""%~0"" hide",0)(window.close)&&exit
+:CmdBegin
+
 @echo off
 setlocal enabledelayedexpansion
 
-:: 关闭子系统
 wsl --shutdown ubuntu
 
-:: WSL 子系统 IP
+::config
 set wsl_ip=192.168.12.18
-:: 网段
 set broadcast=192.168.12.0
-:: Windows 主机IP
 set win_ip=192.168.12.10
+set ports=3000 8080 8888 80 3600
 
+::set network
 if !errorlevel! equ 0 (
-    wsl -u root ip addr | findstr %wsl_ip% > nul
-    if !errorlevel! equ 0 (
-        echo wsl ip has set
-    ) else (
-        wsl -u root ip addr add %wsl_ip%/24 broadcast %broadcast% dev eth0 label eth0:custom
-        echo wsl ip set success
-    )
+  wsl -u root ip addr | findstr %wsl_ip% > nul
+  if !errorlevel! equ 0 (
+    echo wsl ip has set
+  ) else (
+    wsl -u root ip addr add %wsl_ip%/24 broadcast %broadcast% dev eth0 label eth0:custom
+    echo wsl ip set success
+  )
 
-    ipconfig | findstr %win_ip% > nul
-    if !errorlevel! equ 0 (
-        echo windows ip has set
-    ) else (
-        netsh interface ip add address "vEthernet (WSL)" %win_ip% 255.255.255.0
-        echo windows ip set success
-    )
+  ipconfig | findstr %win_ip% > nul
+  if !errorlevel! equ 0 (
+    echo windows ip has set
+  ) else (
+    netsh interface ip add address "vEthernet (WSL)" %win_ip% 255.255.255.0
+    echo windows ip set success
+  )
 )
-pause
+
+::set portproxy
+(for %%a in (%ports%) do (
+  netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=%%a connectaddress=%wsl_ip% connectport=%%a
+))
+
+::pause
